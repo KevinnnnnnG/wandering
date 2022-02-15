07153b576c7f4117c9f6a1b92d33cff9276c4e59
clasehtml <zever469@gmail.com>
2022-02-15 16:03:54 -0500

    Probamos el cambio de usuario

--- wandering.py
+++ wandering.py
@@ -9,6 +9,6 @@ class Comunwandering(Wandering):
         super().__init__(name)
         
     def walk(self):
-        return random.choice([(0,1), (0,-1),(1,0),(-1,0)])
+        return random.choice([(0,2), (0,-2),(2,0),(-2,0)])
     
  
