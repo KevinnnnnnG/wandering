88cc10a26cf176bff5a50fed3b03f6901e96dcd3
clasehtml <zever469@gmail.com>
2022-02-15 15:02:55 -0500

    creamos las clases de wandering y track para el proyecto

---
+++ track.py
@@ -0,0 +1,13 @@
+class Track:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def move(self,delta_x,delta_y):
+        return Track(self.x + delta_x + self.y + delta_y)
+    
+    def distancia(self,other_Track):
+        delta_x = self.x - other_Track.x
+        delta_y = self.y - other_Track.y
+        
+        return (delta_x**2+delta_y**2)**0.5
